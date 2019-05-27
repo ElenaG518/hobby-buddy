2c33acc2146e2e6ce3624a440d41da2f8d47893e
Elena Granados <patty518@yahoo.com>
2019-05-27 14:36:25 -0700

    add Post model

---
+++ models/Posts.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  username: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+      }
+    }
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ],
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
