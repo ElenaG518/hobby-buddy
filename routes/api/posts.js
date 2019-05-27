a04a131b80a250f707b6c5c579139fbc4dc4e6fb
Elena Granados <patty518@yahoo.com>
2019-05-27 14:50:18 -0700

    Add POST endpoint to post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const { check, validationResult } = require('express-validator/check');
 
-// @route    GET api/posts
-// @desc     Test route
-// @access   Public
+const Post = require('../../models/Posts');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('Posts route'));
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+
+router.post(
+  '/',
+  [
+    jwtAuth,
+    [
+      check('content', 'Please add the content to your post')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id);
+
+      const newPost = {
+        user: req.user.id,
+        name: user.name,
+        username: user.username,
+        content: req.body.content,
+        avatar: user.avatar
+      };
+
+      const post = await Post.create(newPost);
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Internal Server Error');
+    }
+  }
+);
 
 module.exports = router;
