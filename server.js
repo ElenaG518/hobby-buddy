ef9284a25d62422a14ad32e115c0d2b3c7603930
Elena Granados <patty518@yahoo.com>
2019-05-27 11:14:46 -0700

    add passport and loggin

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const morgan = require('morgan');
+const passport = require('passport');
+
+const { localStrategy, jwtStrategy } = require('./middleware/strategies');
 const connectDB = require('./config/db');
 const usersRouter = require('./routes/api/users');
 const authRouter = require('./routes/api/auth');
@@ -10,6 +14,14 @@ const app = express();
 // connect to db
 connectDB();
 
+app.use(morgan('common'));
+// app.use(cors());
+// app.use(express.static('public'));
+
+// passport
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
 // Init Middleware
 app.use(express.json({ extended: false })); //in lieu of bodyParser.json, express now has it's own parser.  YAAYY!
 
@@ -20,6 +32,10 @@ app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/profile', profileRouter);
 app.use('/api/posts', postsRouter);
+
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
+});
 
 const PORT = process.env.PORT || 5000;
 
